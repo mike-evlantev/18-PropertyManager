5f5f4b6d34f2c20ef85f00c47fd238f383bb0f7b
Mike Evlantev <mikeev21@gmail.com>
2016-04-09 12:18:23 -0700

    Dashboard items, background opacity

--- 18-PropertyManager/18-PropertyManager.Web/js/app/dashboard/dashboard.ctrl.js
+++ 18-PropertyManager/18-PropertyManager.Web/js/app/dashboard/dashboard.ctrl.js
@@ -1,3 +1,6 @@
-﻿angular.module('app').controller('DashboardController', function ($scope) {
-
+﻿angular.module('app').controller('DashboardController', function ($scope, $state, $stateParams, TenantResource, PropertyResource, LeaseResource, WorkOrderResource) {
+    $scope.tenants = TenantResource.query();
+    $scope.properties = PropertyResource.query();
+    $scope.leases = LeaseResource.query();
+    $scope.workorders = WorkOrderResource.query();
 });
